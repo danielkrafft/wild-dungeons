53a1da98e95b140ec525e0c36609a5ebbd907be7
Skylor Beck <42103122+skylorbeck@users.noreply.github.com>
2025-01-23 08:58:27 -0700

    manually spawning a rift via item now costs 0 xp if the spawner is in creative mode
    Signed-off-by: Skylor Beck <42103122+skylorbeck@users.noreply.github.com>

--- src/main/java/com/danielkkrafft/wilddungeons/item/RiftItem.java
+++ src/main/java/com/danielkkrafft/wilddungeons/item/RiftItem.java
@@ -17,7 +17,8 @@ public class RiftItem extends Item {
     @Override
     public InteractionResult useOn(UseOnContext context) {
         if (context.getLevel().isClientSide) return InteractionResultHolder.pass(context.getPlayer().getItemInHand(context.getHand())).getResult();
-        Offering offering = new Offering(context.getLevel(), Offering.Type.RIFT, 1, "random", Offering.CostType.XP_LEVEL, 2);
+        int cost = context.getPlayer().isCreative() ? 0 : 2;
+        Offering offering = new Offering(context.getLevel(), Offering.Type.RIFT, 1, "random", Offering.CostType.XP_LEVEL, cost);
         Vec3 clickLocation = context.getClickLocation().add(0.0,0.5,0.0);
         offering.setPos(new Vec3(Math.round(clickLocation.x*2.0)/2.0, Math.round(clickLocation.y*2.0)/2.0, Math.round(clickLocation.z*2.0)/2.0));
         WildDungeons.getLogger().info("SPAWNING RIFT AT {}", offering.position());
