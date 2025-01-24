15de96e599f5860c9daa9eb842d4d0b9d308e6b8
Skylor Beck <42103122+skylorbeck@users.noreply.github.com>
2025-01-24 15:41:18 -0700

    removed tick logs
    Signed-off-by: Skylor Beck <42103122+skylorbeck@users.noreply.github.com>

--- src/main/java/com/danielkkrafft/wilddungeons/dungeon/components/DungeonRoom.java
+++ src/main/java/com/danielkkrafft/wilddungeons/dungeon/components/DungeonRoom.java
@@ -291,7 +291,6 @@ public class DungeonRoom {
     }
     public void reset() {}
     public void tick() {
-        WildDungeons.getLogger().info("DUNGEON ROOM TICKING OF CLASS {}", this.getClass().getSimpleName());
         if (this.playerUUIDs.isEmpty()) return;
         innerPlayerUUIDs.forEach((uuid, inside) -> {
             if (!inside) {
