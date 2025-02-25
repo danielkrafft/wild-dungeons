209fc3f458af128b063762902d636edffa39c3cf
Skylor Beck <42103122+skylorbeck@users.noreply.github.com>
2025-02-25 08:22:21 -0800

    semantic changes, totally pointless
    Signed-off-by: Skylor Beck <42103122+skylorbeck@users.noreply.github.com>

--- src/main/java/com/danielkkrafft/wilddungeons/dungeon/components/DungeonMaterial.java
+++ src/main/java/com/danielkkrafft/wilddungeons/dungeon/components/DungeonMaterial.java
@@ -56,34 +56,34 @@ public class DungeonMaterial implements DungeonRegistration.DungeonComponent {
     public BlockState replace(BlockState input) {
         BlockState result = input;
 
-        if (input.getBlock() == WDBlocks.WD_BASIC.get()) {result = getBasic(0);}
-        else if (input.getBlock() == WDBlocks.WD_BASIC_2.get()) {result = getBasic(1);}
-        else if (input.getBlock() == WDBlocks.WD_BASIC_3.get()) {result = getBasic(2);}
-        else if (input.getBlock() == WDBlocks.WD_BASIC_4.get()) {result = getBasic(3);}
-        else if (input.getBlock() == WDBlocks.WD_STAIRS.get()) {result = getStair(0);}
-        else if (input.getBlock() == WDBlocks.WD_STAIRS_2.get()) {result = getStair(1);}
-        else if (input.getBlock() == WDBlocks.WD_STAIRS_3.get()) {result = getStair(2);}
-        else if (input.getBlock() == WDBlocks.WD_STAIRS_4.get()) {result = getStair(3);}
-        else if (input.getBlock() == WDBlocks.WD_SLAB.get()) {result = getSlab(0);}
-        else if (input.getBlock() == WDBlocks.WD_SLAB_2.get()) {result = getSlab(1);}
-        else if (input.getBlock() == WDBlocks.WD_SLAB_3.get()) {result = getSlab(2);}
-        else if (input.getBlock() == WDBlocks.WD_SLAB_4.get()) {result = getSlab(3);}
-        else if (input.getBlock() == WDBlocks.WD_WALL.get()) {result = getWall(0);}
-        else if (input.getBlock() == WDBlocks.WD_WALL_2.get()) {result = getWall(1);}
-        else if (input.getBlock() == WDBlocks.WD_WALL_3.get()) {result = getWall(2);}
-        else if (input.getBlock() == WDBlocks.WD_WALL_4.get()) {result = getWall(3);}
-        else if (input.getBlock() == WDBlocks.WD_LIGHT.get()) {result = getLight(0);}
-        else if (input.getBlock() == WDBlocks.WD_LIGHT_2.get()) {result = getLight(1);}
-        else if (input.getBlock() == WDBlocks.WD_LIGHT_3.get()) {result = getLight(2);}
-        else if (input.getBlock() == WDBlocks.WD_LIGHT_4.get()) {result = getLight(3);}
-        else if (input.getBlock() == WDBlocks.WD_HANGING_LIGHT.get()) {result = getHangingLight(0).trySetValue(BlockStateProperties.HANGING,true);}
-        else if (input.getBlock() == WDBlocks.WD_HANGING_LIGHT_2.get()) {result = getHangingLight(1).trySetValue(BlockStateProperties.HANGING,true);}
-        else if (input.getBlock() == WDBlocks.WD_HANGING_LIGHT_3.get()) {result = getHangingLight(2).trySetValue(BlockStateProperties.HANGING,true);}
-        else if (input.getBlock() == WDBlocks.WD_HANGING_LIGHT_4.get()) {result = getHangingLight(3).trySetValue(BlockStateProperties.HANGING,true);}
-        else if (input.getBlock() == WDBlocks.WD_SECRET.get()) {result = getHidden(0);}
+        if (input.is(WDBlocks.WD_BASIC.get())) {result = getBasic(0);}
+        else if (input.is(WDBlocks.WD_BASIC_2.get())) {result = getBasic(1);}
+        else if (input.is(WDBlocks.WD_BASIC_3.get())) {result = getBasic(2);}
+        else if (input.is(WDBlocks.WD_BASIC_4.get())) {result = getBasic(3);}
+        else if (input.is(WDBlocks.WD_STAIRS.get())) {result = getStair(0);}
+        else if (input.is(WDBlocks.WD_STAIRS_2.get())) {result = getStair(1);}
+        else if (input.is(WDBlocks.WD_STAIRS_3.get())) {result = getStair(2);}
+        else if (input.is(WDBlocks.WD_STAIRS_4.get())) {result = getStair(3);}
+        else if (input.is(WDBlocks.WD_SLAB.get())) {result = getSlab(0);}
+        else if (input.is(WDBlocks.WD_SLAB_2.get())) {result = getSlab(1);}
+        else if (input.is(WDBlocks.WD_SLAB_3.get())) {result = getSlab(2);}
+        else if (input.is(WDBlocks.WD_SLAB_4.get())) {result = getSlab(3);}
+        else if (input.is(WDBlocks.WD_WALL.get())) {result = getWall(0);}
+        else if (input.is(WDBlocks.WD_WALL_2.get())) {result = getWall(1);}
+        else if (input.is(WDBlocks.WD_WALL_3.get())) {result = getWall(2);}
+        else if (input.is(WDBlocks.WD_WALL_4.get())) {result = getWall(3);}
+        else if (input.is(WDBlocks.WD_LIGHT.get())) {result = getLight(0);}
+        else if (input.is(WDBlocks.WD_LIGHT_2.get())) {result = getLight(1);}
+        else if (input.is(WDBlocks.WD_LIGHT_3.get())) {result = getLight(2);}
+        else if (input.is(WDBlocks.WD_LIGHT_4.get())) {result = getLight(3);}
+        else if (input.is(WDBlocks.WD_HANGING_LIGHT.get())) {result = getHangingLight(0).trySetValue(BlockStateProperties.HANGING,true);}
+        else if (input.is(WDBlocks.WD_HANGING_LIGHT_2.get())) {result = getHangingLight(1).trySetValue(BlockStateProperties.HANGING,true);}
+        else if (input.is(WDBlocks.WD_HANGING_LIGHT_3.get())) {result = getHangingLight(2).trySetValue(BlockStateProperties.HANGING,true);}
+        else if (input.is(WDBlocks.WD_HANGING_LIGHT_4.get())) {result = getHangingLight(3).trySetValue(BlockStateProperties.HANGING,true);}
+        else if (input.is(WDBlocks.WD_SECRET.get())) {result = getHidden(0);}
         //IMPORTANT: do NOT replace Trapped Chests as this will break some rooms
-        else if (input.getBlock() == Blocks.CHEST) {result = RandomUtil.randFloatBetween(0, 1) < chestChance ? Blocks.CHEST.defaultBlockState() : Blocks.AIR.defaultBlockState();}
-        else if (input.getBlock() == Blocks.BARREL) {result = RandomUtil.randFloatBetween(0, 1) < chestChance ? Blocks.BARREL.defaultBlockState() : Blocks.AIR.defaultBlockState();}
+        else if (input.is(Blocks.CHEST)) {result = RandomUtil.randFloatBetween(0, 1) < chestChance ? Blocks.CHEST.defaultBlockState() : Blocks.AIR.defaultBlockState();}
+        else if (input.is(Blocks.BARREL)) {result = RandomUtil.randFloatBetween(0, 1) < chestChance ? Blocks.BARREL.defaultBlockState() : Blocks.AIR.defaultBlockState();}
         else {return result;}
 
         for (Property<?> property : input.getProperties()) {
