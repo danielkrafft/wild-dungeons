ca327aa66e983729dd69016c9a9e8f6e2026fff6
Skylor Beck <42103122+skylorbeck@users.noreply.github.com>
2025-05-09 16:56:09 -0700

    Fixed boss spawning randomly because I changed the signature of the method
    Signed-off-by: Skylor Beck <42103122+skylorbeck@users.noreply.github.com>

--- src/main/java/com/danielkkrafft/wilddungeons/dungeon/components/room/BossRoom.java
+++ src/main/java/com/danielkkrafft/wilddungeons/dungeon/components/room/BossRoom.java
@@ -7,6 +7,7 @@ import com.danielkkrafft.wilddungeons.dungeon.components.template.TemplateHelper
 import com.danielkkrafft.wilddungeons.dungeon.components.template.TemplateOrientation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
@@ -20,6 +21,11 @@ public class BossRoom extends CombatRoom {
     @Override
     public List<DungeonRegistration.TargetTemplate> getTargetTemplates() {
         return this.getProperty(HierarchicalProperty.ENEMY_TABLE).randomResults(1,1,1);
+    }
+
+    @Override
+    public List<BlockPos> sampleSpawnablePositions(ServerLevel serverLevel, int count, EntityType<?> mobType) {
+        return List.of((TemplateHelper.transform(BlockPos.containing(this.getProperty(HierarchicalProperty.BOSS_SPAWN_POS)), this)));
     }
 
     @Override
