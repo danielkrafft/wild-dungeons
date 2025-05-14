216c1e16f54b1ca00219bcf06300313b12b7429f
Skylor Beck <42103122+skylorbeck@users.noreply.github.com>
2025-05-14 11:13:23 -0700

    Custom Village Well added to the pool that fixes the rotation issue, rift color issue, and rift cost as per #140
    Signed-off-by: Skylor Beck <42103122+skylorbeck@users.noreply.github.com>

--- src/main/java/com/danielkkrafft/wilddungeons/dungeon/registries/DungeonPoolRegistry.java
+++ src/main/java/com/danielkkrafft/wilddungeons/dungeon/registries/DungeonPoolRegistry.java
@@ -5,7 +5,7 @@ import com.danielkkrafft.wilddungeons.util.WeightedPool;
 
 import static com.danielkkrafft.wilddungeons.dungeon.registries.DungeonRegistry.*;
 
-public class DungeonPoolRegistry {
+public class DungeonPoolRegistry {//is this redundant? We have RiftPoolRegistry and this seems outdated and has no uses
     public static final WeightedPool<DungeonTemplate> TEST_DUNGEON_POOL = new WeightedPool<DungeonTemplate>()
             .add(TEST_DUNGEON, 1);;
     public static final WeightedPool<DungeonTemplate> OVERWORLD_DUNGEON_POOL = new WeightedPool<DungeonTemplate>()
